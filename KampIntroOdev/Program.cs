5dffe6ba97c59376bba88bad82ab6b0ca539ff80
CemalFDogan <cxexmxaxl4409@gmail.com>
2022-08-10 14:19:50 +0300

    Add project files.

---
+++ KampIntroOdev/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KampIntroOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
